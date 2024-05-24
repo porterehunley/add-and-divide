0811742f5d84d8b4d2956a20480b6db897746814
Porter Hunley <porterehunley@gmail.com>
2024-05-24 08:57:08 -0400

    factor out expense add

--- add-and-divide/src/app/group/[id]/page.tsx
+++ add-and-divide/src/app/group/[id]/page.tsx
@@ -9,10 +9,9 @@ import {
   getGroupWithChildren,
   group,
   addMemberToGroup,
-  addExpenseToMember,
-  expense,
   member
 } from '@/dbopps';
+import ExpenseAdd from '@/components/ExpenseAdd';
 import MemberSelection from '@/components/MemberSelection';
 import CurrencyInput from 'react-currency-input-field';
 import '@/app/globals.css';
@@ -34,30 +33,6 @@ export default function Group({ params }: { params: { id: string } }) {
     setGroupData(data);
   };
 
-  const validateValue = (value: string | undefined): void => {
-    const rawValue = value === undefined ? 'undefined' : value;
-    if (Number.isNaN(rawValue)) {
-      return;
-    }
-
-    setExpenseAmmount(Number(rawValue));
-  }
-
-  const addExpenseClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    const memberId = selectedMember?.id;
-    if (!expenseName || !expenseAmmount || !memberId) {
-      console.log("data not set");
-      return
-    }
-
-    const expense: expense = {
-      title: expenseName,
-      ammount: expenseAmmount
-    }
-
-    await addExpenseToMember(groupId, memberId, expense);
-  }
 
   const addNewMemberClick = async (name: string) => {
     if (groupData === undefined) {
@@ -80,9 +55,12 @@ export default function Group({ params }: { params: { id: string } }) {
   }, [groupId])
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen bg-[#f0f0f5] dark:bg-[#1a1a2e]">
-      <div className="w-full h-full max-w-md p-6 bg-white rounded-lg shadow-lg dark:bg-[#2c2c54]">
-        <h1 className="text-2xl font-bold mb-4 text-center text-[#6b5b95]">{groupData?.name}</h1>
+    <main className="flex flex-col items-center justify-center 
+      h-screen bg-[#f0f0f5] dark:bg-[#1a1a2e]">
+      <div className="w-full h-full max-w-md p-6 bg-white rounded-lg 
+        shadow-lg dark:bg-[#2c2c54]">
+        <h1 className="text-2xl font-bold mb-4 text-center 
+          text-[#6b5b95]">{groupData?.name}</h1>
         <div className="space-y-4">
           <div className="
             space-y-2 border-2 border-[#e6e6e6] 
@@ -112,36 +90,10 @@ export default function Group({ params }: { params: { id: string } }) {
           <div className="space-y-2">
           </div>
           <div className="border-t border-[#e6e6e6] dark:border-[#3c3c58] pt-4">
-            <form className="space-y-4">
-              <div className="flex items-center gap-2 flex-col">
-                <Input
-                  className="flex-1 border-[#e6e6e6] dark:border-[#3c3c58] 
-                    bg-[#f0f0f5] dark:bg-[#2c2c54] text-[#6b5b95]"
-                  placeholder="Expense Name"
-                  value={expenseName}
-                  onChange={(e) => setExpenseName(e.target.value)}
-                  type="text"
-                />
-                <div className='w-full justify-between flex'>
-                  <CurrencyInput 
-                    placeholder="$42"
-                    allowDecimals={false}
-                    onValueChange={validateValue}
-                    className='border border-[#e6e6e6] dark:border-[#3c3c58] 
-                    bg-[#f0f0f5] dark:bg-[#2c2c54] text-[#6b5b95] px-3 py-2
-                    rounded-md h-10 focus-visible:ring-2 focus-visible:ring-gray-950
-                    ring-offset-white focus-visible:outline-none text-sm
-                    focus-visible:ring-offset-2 placeholder:text-gray-400'
-                    prefix={'$'}
-                    step={10}
-                  />
-                  <Button
-                    disabled={!expenseName?.trim() || !selectedMember?.id || !expenseAmmount}
-                    onClick={(e) => addExpenseClick(e)}
-                    className="bg-[#9370db] hover:bg-[#8258fa] text-white">Add</Button>
-                </div>
-              </div>
-            </form>
+            <ExpenseAdd 
+              groupId={groupId}
+              memberId={selectedMember?.id}
+            />
           </div>
         </div>
       </div>
---
+++ add-and-divide/src/components/ExpenseAdd.tsx
@@ -0,0 +1,79 @@
+// Loading
+// Adding
+// disabling
+import { useState } from 'react';
+import CurrencyInput from 'react-currency-input-field';
+import { expense } from "@/dbopps";
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { addExpenseToMember } from '@/dbopps';
+
+interface ExpenseAddProps {
+  groupId: string,
+  memberId?: string,
+  onExpenseAdd?: (expense: expense) => Promise<void>
+}
+
+export default function ExpenseAdd({ 
+    memberId, groupId, onExpenseAdd 
+  }: ExpenseAddProps) {
+  const [expenseAmmount, setExpenseAmmount] = useState<number>();
+  const [expenseName, setExpenseName] = useState<string>();
+  const [isLoading, setIsLoading] = useState<boolean>();
+
+  const validateValue = (value: string | undefined): void => {
+    const rawValue = value === undefined ? 'undefined' : value;
+    if (Number.isNaN(rawValue)) {
+      return;
+    }
+
+    setExpenseAmmount(Number(rawValue));
+  }
+
+  const addExpenseClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!expenseName || !expenseAmmount || !memberId) {
+      console.log("data not set");
+      return
+    }
+
+    const expense: expense = {
+      title: expenseName,
+      ammount: expenseAmmount
+    }
+    setIsLoading(true);
+    await addExpenseToMember(groupId, memberId, expense);
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="flex items-center gap-2 flex-col">
+      <Input
+        className="flex-1 border-[#e6e6e6] dark:border-[#3c3c58] 
+          bg-[#f0f0f5] dark:bg-[#2c2c54] text-[#6b5b95]"
+        placeholder="Expense Name"
+        value={expenseName}
+        onChange={(e) => setExpenseName(e.target.value)}
+        type="text"
+      />
+      <div className='w-full justify-between flex'>
+        <CurrencyInput 
+          placeholder="$42"
+          allowDecimals={false}
+          onValueChange={validateValue}
+          className='border border-[#e6e6e6] dark:border-[#3c3c58] 
+          bg-[#f0f0f5] dark:bg-[#2c2c54] text-[#6b5b95] px-3 py-2
+          rounded-md h-10 focus-visible:ring-2 focus-visible:ring-gray-950
+          ring-offset-white focus-visible:outline-none text-sm
+          focus-visible:ring-offset-2 placeholder:text-gray-400'
+          prefix={'$'}
+          step={10}
+        />
+        <Button
+          disabled={!expenseName?.trim() || !memberId || !expenseAmmount}
+          onClick={(e) => addExpenseClick(e)}
+          className="bg-[#9370db] hover:bg-[#8258fa] text-white">Add</Button>
+      </div>
+    </div>
+  );
+}
