8071ac579ce2afaee08875a28335af379766eca2
Porter Hunley <porterehunley@gmail.com>
2024-06-08 11:39:12 -0400

    working nav

--- add-and-divide/src/components/ui/GroupSection.tsx
+++ add-and-divide/src/components/ui/GroupSection.tsx
@@ -1,5 +1,6 @@
 import { GroupReference } from "@/dbopps";
 import { useRouter } from "next/navigation";
+import RightChevronIcon from "./RigthChevronIcon";
 
 export interface GroupSectionProps {
   group: GroupReference 
@@ -9,10 +10,13 @@ export default function GroupSection({group}: GroupSectionProps) {
   const router = useRouter();
 
   return (
-    <div className="border-t border-[#e6e6e6] pb-4">
-      <h2 className="text-md text-[#6b5b95]">
+    <div 
+      onClick={() => router.push(`/group/${group.groupId}`)}
+      className="border-t border-[#e6e6e6] py-4 cursor-pointer flex flex-row justify-between">
+      <h2 className="text-md text-[#6b5b95] text-left">
         {group.name}
       </h2>
+      <RightChevronIcon className='h-6 w-6 text-[#6b5b95] cursor-pointer' />
     </div>
   );
 }
